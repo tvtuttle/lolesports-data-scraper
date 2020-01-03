5da604c8b61805f15a13f05195d911f3e1354671
tvtuttle <tvtuttle97@gmail.com>
2020-01-03 15:34:47 -0800

    created file for excel spreadsheet processing and io

---
+++ fantasy-spreadsheet-io.py
@@ -0,0 +1,6 @@
+# this file is for developing the format for input and output
+# in both cases, there will be an excel spreadsheet with information regarding fantasy team and player choices
+# after input, the spreadsheet will be filled with stats (from scraper), calculate fantasy points, and return a
+# resulting sheet
+
+
