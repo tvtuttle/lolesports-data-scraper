9c296d16ba10b77312e995efb7eecdd3d7a6f4ce
tvtuttle <tvtuttle97@gmail.com>
2020-01-22 14:58:57 -0800

    began work on text input functionality

---
+++ fantasy_lcs_main.py
@@ -0,0 +1,33 @@
+# this file will act as the true main eventually
+# want to include ui or user input prompts to make program easily useable, esp. outside ide
+# will prompt user (in some way) for information, then send that info to spreadsheet io
+# for now, focus on command line input w/ input function
+
+if __name__ == "__main__":
+    # prompt for user input
+    league = input("Welcome to Fantasy LCS. Please choose a league (LCS, LEC): ")
+    league = league.upper()
+    while league not in ['LCS', 'LEC']:
+        league = input("Invalid league. Please select a supported league (LCS, LEC): ")
+        league = league.upper()
+
+    year = input("Please choose a year (ex. 2019): ")
+    while year not in ['2019', '2020']:
+        year = input("Invalid year. Please select a supported year (2019, 2020): ")
+
+    season = input("Please choose a season (Spring, Summer): ")
+    season = season.lower()
+    season = season.capitalize()
+    while season not in ['Spring', 'Summer']:
+        season = input("Invalid season. Please select a supported season (Spring, Summer): ")
+        season = season.lower()
+        season = season.capitalize()
+    week = input("Please choose a week number: ")
+    while int(week) not in range(1, 10):
+        week = input("Invalid number. Please select a supported week (1 through 9): ")
+
+    # this requires special seasoning
+    input_path = input("Please enter the location of you fantasy team data: ")
+
+    print(league + " " + year + " " + season + ", Week " + week)
+
